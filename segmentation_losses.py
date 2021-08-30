0e05a97ef50887228b1fadf8427cf3fe2a18525b
Tamer Tahamoqa <tamerthamoqa@gmail.com>
2021-08-30 19:03:18 +0300

    Modify binary segmentation losses and metrics to be separate functions in segmentation_losses.py

--- segmentation_losses.py
+++ segmentation_losses.py
@@ -1,8 +1,8 @@
 """This module was originally imported from the reference implementation in the Week 3 of Course 1 project in the
 AI for Medicine Specialization course on Coursera.
 
-shruti-jadon's 'Semantic-Segmentation-Loss-Functions' GitHub repository binary segmentation loss functions were
- imported for binary training mode:
+The binary segmentation losses and metric were imported from shruti-jadon's 'Semantic-Segmentation-Loss-Functions'
+ GitHub repository for binary segmentation training mode:
  https://github.com/shruti-jadon/Semantic-Segmentation-Loss-Functions/blob/master/loss_functions.py
 """
 
@@ -10,7 +10,7 @@ import tensorflow.keras.backend as K
 import tensorflow as tf
 
 
-def dice_coefficient(y_true, y_pred, axis=(0, 1, 2, 3), binary_training=False):
+def dice_coefficient(y_true, y_pred, axis=(0, 1, 2, 3)):
     """
     Compute mean dice coefficient over all classes.
 
@@ -21,28 +21,39 @@ def dice_coefficient(y_true, y_pred, axis=(0, 1, 2, 3), binary_training=False):
                  shape: (batch_size, x_dim, y_dim, z_dim, num_classes)
         axis (tuple): Spatial axes to sum over when computing numerator and
                       denominator in formula for dice coefficient.
-        binary_training: (bool) If True, set the dice coefficient implementation suitable to binary segmentation
-                      training.
     Returns:
-        dice_coefficient (float): Mean value of dice dice_coefficient over all classes.
+        dice_coefficient (float): Mean value of dice coefficient over all classes.
     """
-    if binary_training:
-        y_true_f = K.flatten(y_true)
-        y_pred_f = K.flatten(y_pred)
-        intersection = K.sum(y_true_f * y_pred_f)
+    dice_numerator = 2 * K.sum(y_pred * y_true, axis=axis) + K.epsilon()
+    dice_denominator = K.sum(y_pred ** 2, axis=axis) + K.sum(y_true ** 2, axis=axis) + K.epsilon()
 
-        return (2. * intersection + K.epsilon()) / (
-                K.sum(y_true_f) + K.sum(y_pred_f) + K.epsilon())
-    else:
-        dice_numerator = 2 * K.sum(y_pred * y_true, axis=axis) + K.epsilon()
-        dice_denominator = K.sum(y_pred ** 2, axis=axis) + K.sum(y_true ** 2, axis=axis) + K.epsilon()
+    dice_coefficient = K.mean(dice_numerator / dice_denominator)
 
-        dice_coefficient = K.mean(dice_numerator / dice_denominator)
-
-        return dice_coefficient
+    return dice_coefficient
 
 
-def dice_loss(y_true, y_pred, axis=(0, 1, 2, 3), binary_training=False):
+def dice_coefficient_binary(y_true, y_pred):
+    """
+    Compute dice coefficient (binary segmentation).
+
+    Args:
+        y_true: Ground truth values for all classes.
+                 shape: (batch_size, x_dim, y_dim, z_dim)
+        y_pred: Predictions for all classes.
+                 shape: (batch_size, x_dim, y_dim, z_dim)
+
+    Returns:
+        dice_coefficient (float): Value of dice coefficient.
+    """
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+
+    return (2. * intersection + K.epsilon()) / (
+            K.sum(y_true_f) + K.sum(y_pred_f) + K.epsilon())
+
+
+def dice_loss(y_true, y_pred, axis=(0, 1, 2, 3)):
     """
     Compute mean dice loss over all classes.
 
@@ -53,22 +64,37 @@ def dice_loss(y_true, y_pred, axis=(0, 1, 2, 3), binary_training=False):
                  shape: (batch_size, x_dim, y_dim, z_dim, num_classes)
         axis (tuple): Spatial axes to sum over when computing numerator and
                       denominator in formula for dice loss.
-        binary_training: (bool) If True, set the dice coefficient implementation suitable to binary segmentation
-                      training.
     Returns:
         dice_loss (float): Mean value of dice loss over all classes.
     """
     dice_loss = 1 - dice_coefficient(
         y_true=y_true,
         y_pred=y_pred,
-        axis=axis,
-        binary_training=binary_training
+        axis=axis
     )
 
     return dice_loss
 
 
-def log_cosh_dice_loss(y_true, y_pred, axis=(0, 1, 2, 3), binary_training=False):
+def dice_loss_binary(y_true, y_pred):
+    """
+    Compute dice loss (binary segmentation).
+
+    Args:
+        y_true: Ground truth values for all classes.
+                 shape: (batch_size, x_dim, y_dim, z_dim)
+        y_pred: Predictions for all classes.
+                 shape: (batch_size, x_dim, y_dim, z_dim)
+
+    Returns:
+        dice_loss (float): Value of dice loss.
+    """
+    loss = 1 - dice_coefficient_binary(y_true, y_pred)
+
+    return loss
+
+
+def log_cosh_dice_loss(y_true, y_pred, axis=(0, 1, 2, 3)):
     """
     Compute mean log cosh dice loss over all classes.
 
@@ -79,22 +105,37 @@ def log_cosh_dice_loss(y_true, y_pred, axis=(0, 1, 2, 3), binary_training=False)
                  shape: (batch_size, x_dim, y_dim, z_dim, num_classes)
         axis (tuple): Spatial axes to sum over when computing numerator and
                       denominator in formula for dice loss.
-        binary_training: (bool) If True, set the dice coefficient implementation suitable to binary segmentation
-                      training.
     Returns:
         dice_loss (float): Mean value of dice loss over all classes.
     """
     x = 1 - dice_coefficient(
         y_true=y_true,
         y_pred=y_pred,
-        axis=axis,
-        binary_training=binary_training
+        axis=axis
     )
 
     return tf.math.log((tf.exp(x) + tf.exp(-x)) / 2.0)
 
 
-def iou(y_true, y_pred, smooth=1., axis=(0, 1, 2, 3), binary_training=False):
+def log_cosh_dice_loss_binary(y_true, y_pred):
+    """
+    Compute log cosh dice loss (binary segmentation).
+
+    Args:
+        y_true: Ground truth values for all classes.
+                 shape: (batch_size, x_dim, y_dim, z_dim)
+        y_pred: Predictions for all classes.
+                 shape: (batch_size, x_dim, y_dim, z_dim)
+
+    Returns:
+        log_cosh_dice_loss (float): Value of log cosh dice loss.
+    """
+    x = dice_loss_binary(y_true, y_pred)
+
+    return tf.math.log((tf.exp(x) + tf.exp(-x)) / 2.0)
+
+
+def iou(y_true, y_pred, smooth=1., axis=(0, 1, 2, 3)):
     """Compute mean intersection over union (iou) over all classes.
 
     Args:
@@ -104,18 +145,31 @@ def iou(y_true, y_pred, smooth=1., axis=(0, 1, 2, 3), binary_training=False):
                  shape: (batch_size, x_dim, y_dim, z_dim, num_classes)
         axis (tuple): Spatial axes to sum over when computing numerator and
                       denominator in formula for iou.
-        binary_training: (bool) If True, set the dice coefficient implementation suitable to binary segmentation
-                      training.
     Returns:
         iou (float): Mean value of iou over all classes.
     """
-    if binary_training:
-        y_true_f = K.flatten(y_true)
-        y_pred_f = K.flatten(y_pred)
-        intersection = K.sum(y_true_f * y_pred_f)
+    intersection = K.mean(K.sum(y_true * y_pred, axis=axis))
 
-        return (intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + smooth)
-    else:
-        intersection = K.mean(K.sum(y_true * y_pred, axis=axis))
+    return (intersection + smooth) / (K.mean(K.sum(y_true, axis=axis)) + K.mean(K.sum(y_pred, axis=axis)) - intersection + smooth)
 
-        return (intersection + smooth) / (K.mean(K.sum(y_true, axis=axis)) + K.mean(K.sum(y_pred, axis=axis)) - intersection + smooth)
+
+def iou_binary(y_true, y_pred, smooth=1.):
+    """
+    Compute intersection over union metric (binary segmentation).
+    Added from: https://github.com/karolzak/keras-unet/blob/master/keras_unet/metrics.py
+
+    Args:
+        y_true: Ground truth values for all classes.
+                 shape: (batch_size, x_dim, y_dim, z_dim)
+        y_pred: Predictions for all classes.
+                 shape: (batch_size, x_dim, y_dim, z_dim)
+        smooth: Smoothing parameters. (default: 1.)
+
+    Returns:
+        iou_binary (float): Value of iou metric.
+    """
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+
+    return (intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + smooth)
